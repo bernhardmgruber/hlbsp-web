9975884031db5cec48cbd2a5736b5e5d1bbad649
Bernhard Manfred Gruber <dixxi1@gmx.net>
2012-01-05 02:03:05 +0100

    > Included readme and license > Included license header in every js source file.

--- js/binfile.js
+++ js/binfile.js
@@ -1,9 +1,31 @@
+/*
+ * binfile.js
+ * 
+ * Copyright (c) 2012, Bernhard Manfred Gruber. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
 'use strict';
  
  /**
  * Class for reading c-style datatypes from a raw ArrayBuffer obtained by reading a binary file.
  * The following code has been inspirated by Brandon Jones's High performance Binary Reader
  * http://media.tojicode.com/q3bsp/js/common/binFile_man.js
+ * but has been rewritten using the DataView class.
  */
 function BinaryFile(arrayBuffer)
 {
